db24da6cbc9ac8520b17e0772f32dd8b208c540a
Xinlin Long <xlon323@aucklanduni.ac.nz>
2023-09-13 21:32:06 +1200

    Created interface Mission and class MissionManager to manage all Mission fot the game

---
+++ src/main/java/nz/ac/auckland/se206/MissionManager.java
@@ -0,0 +1,8 @@
+package nz.ac.auckland.se206;
+
+import java.util.HashMap;
+import nz.ac.auckland.se206.missions.Mission;
+
+public class MissionManager {
+  private HashMap<Integer, Mission> missionList = new HashMap<>();
+}
---
+++ src/main/java/nz/ac/auckland/se206/missions/Mission.java
@@ -0,0 +1,3 @@
+package nz.ac.auckland.se206.missions;
+
+public interface Mission {}
