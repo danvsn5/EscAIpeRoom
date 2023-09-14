db35dd00004ea86e830fd1f38783ec96507ab87c
Xinlin Long <xlon323@aucklanduni.ac.nz>
2023-09-14 19:35:42 +1200

    Modified class MissionManager, created enum MISSION which contains all missions and implemented the addMission method to add mission to hash map according to input integer

--- src/main/java/nz/ac/auckland/se206/MissionManager.java
+++ src/main/java/nz/ac/auckland/se206/MissionManager.java
@@ -2,7 +2,18 @@ package nz.ac.auckland.se206;
 
 import java.util.HashMap;
 import nz.ac.auckland.se206.missions.Mission;
+import nz.ac.auckland.se206.missions.WindowMission;
 
 public class MissionManager {
-  private HashMap<Integer, Mission> missionList = new HashMap<>();
+  public enum MISSION {
+    WINDOW,
+  }
+
+  private HashMap<MISSION, Mission> missionList = new HashMap<>();
+
+  public void addMission(int missionNumber) {
+    if (missionNumber == 1) {
+      missionList.put(MISSION.WINDOW, new WindowMission());
+    }
+  }
 }
