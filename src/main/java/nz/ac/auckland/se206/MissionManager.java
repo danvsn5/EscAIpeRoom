f2ef474d283b2297b389eca4e65423127ab68102
Xinlin Long <xlon323@aucklanduni.ac.nz>
2023-09-16 14:40:50 +1200

    Implemented method getOverallPercentage to calculate the total percentage of all missions

--- src/main/java/nz/ac/auckland/se206/MissionManager.java
+++ src/main/java/nz/ac/auckland/se206/MissionManager.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se206;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import nz.ac.auckland.se206.missions.Mission;
 import nz.ac.auckland.se206.missions.WindowMission;
@@ -10,10 +11,20 @@ public class MissionManager {
   }
 
   private HashMap<MISSION, Mission> missionList = new HashMap<>();
+  private ArrayList<MISSION> keyList = new ArrayList<>();
 
   public void addMission(int missionNumber) {
     if (missionNumber == 1) {
       missionList.put(MISSION.WINDOW, new WindowMission());
+      keyList.add(MISSION.WINDOW);
     }
+  }
+
+  public int getOverallPercentage() {
+    int percentage = 0;
+    for (MISSION m : keyList) {
+      percentage += missionList.get(m).getPercentage();
+    }
+    return percentage / keyList.size();
   }
 }
