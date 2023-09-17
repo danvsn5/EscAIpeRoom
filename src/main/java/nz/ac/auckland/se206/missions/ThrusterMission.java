a46f2c6efd2d0ebdf692f238b0766b02b3ad6ad0
Xinlin Long <xlon323@aucklanduni.ac.nz>
2023-09-17 19:41:56 +1200

    Added ThrusterMission class which is a mission contains 3 stages

---
+++ src/main/java/nz/ac/auckland/se206/missions/ThrusterMission.java
@@ -0,0 +1,48 @@
+package nz.ac.auckland.se206.missions;
+
+import nz.ac.auckland.se206.gpt.ChatMessage;
+
+public class ThrusterMission implements Mission {
+  private int currentStage;
+  private int totalStage;
+
+  public ThrusterMission() {
+    currentStage = 0;
+    totalStage = 3;
+  }
+
+  @Override
+  public void initialize() {
+    // TODO Set the correct image to visible
+  }
+
+  @Override
+  public String getName() {
+    return "Fix the thrusters of the ship";
+  }
+
+  @Override
+  public void increaseStage() {
+    currentStage++;
+    if (currentStage > totalStage) {
+      System.out.println("Mission already finished, can't increase stage");
+      currentStage--;
+    }
+  }
+
+  @Override
+  public int getStage() {
+    return currentStage;
+  }
+
+  @Override
+  public int getPercentage() {
+    return currentStage * 100 / totalStage;
+  }
+
+  @Override
+  public ChatMessage askGpt() {
+    // TODO ask gpt to generate riddle
+    throw new UnsupportedOperationException("Unimplemented method 'askGpt'");
+  }
+}
