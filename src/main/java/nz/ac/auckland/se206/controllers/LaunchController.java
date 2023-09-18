140b33042b4ee266a8720dbd457a780d6a4a9d89
dvas511 <dvas511@aucklanduni.ac.nz>
2023-09-19 09:41:16 +1200

    removed timer countdown from command terminal for cleaner terminal output

--- src/main/java/nz/ac/auckland/se206/controllers/LaunchController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LaunchController.java
@@ -118,7 +118,6 @@ public class LaunchController {
               if (!timer.isEnd()) {
                 Platform.runLater(
                     () -> {
-                      System.out.println(timer.getTime());
                       updateClock(timer.getTime());
                     });
               }
