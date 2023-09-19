d1fe4de6e419212999d134980a650813028da57e
Xinlin Long <xlon323@aucklanduni.ac.nz>
2023-09-20 11:10:44 +1200

    Added three labels which shows three digits of the lock and the button to indrease/ decrease them in chest panel

--- src/main/java/nz/ac/auckland/se206/controllers/ChestController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ChestController.java
@@ -2,13 +2,31 @@ package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager.AppPanel;
 
 public class ChestController {
   @FXML private Button backButton;
+  @FXML private Button firstDigitUpButton;
+  @FXML private Button firstDigitDownButton;
+  @FXML private Button secondDigitUpButton;
+  @FXML private Button secondDigitDownButton;
+  @FXML private Button thirdDigitUpButton;
+  @FXML private Button thirdDigitDownButton;
+  @FXML private Label firstDigit;
+  @FXML private Label secondDigit;
+  @FXML private Label thirdDigit;
+
+  private int firstDigitNum = 0;
+  private int secondDigitNum = 0;
+  private int thirdDigitNum = 0;
 
   public void goBack() {
     App.setUi(AppPanel.MAIN_ROOM);
+  }
+
+  public void firstDigitUp() {
+    firstDigitNum++;
   }
 }
