3f134ee6d82f2450d9a87bca9e0b29d512a23476
dvas511 <dvas511@aucklanduni.ac.nz>
2023-09-20 03:42:35 +1200

    instead of using different image views for different coloured buttons, created new setHue effect. Timeline now changes the color of one button rather than altering the visibility of different images

--- src/main/java/nz/ac/auckland/se206/ThrusterButtons.java
+++ src/main/java/nz/ac/auckland/se206/ThrusterButtons.java
@@ -2,58 +2,51 @@ package nz.ac.auckland.se206;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.effect.Effect;
 import javafx.util.Duration;
 import nz.ac.auckland.se206.SceneManager.AppPanel;
 
 public class ThrusterButtons {
 
-  public enum ThrusterButton {
-    BOTTOM_LEFT_PURPLE_UNCLICKED,
-    BOTTOM_LEFT_ORANGE_UNCLICKED,
-    BOTTOM_LEFT_BLUE_UNCLICKED,
-    BOTTOM_LEFT_RED_UNCLICKED,
-    BOTTOM_LEFT_PURPLE_CLICKED,
-    BOTTOM_LEFT_ORANGE_CLICKED,
-    BOTTOM_LEFT_BLUE_CLICKED,
-    BOTTOM_LEFT_RED_CLICKED
-  }
-
   public static Timeline timeline =
       new Timeline(new KeyFrame(Duration.millis(1000), e -> cycleBottomLeft()));
   private static int cycleNumber = 1;
+  private static ColorAdjust purpleColor = new ColorAdjust(0, 0, 0, 0);
+  private static ColorAdjust greenColor = new ColorAdjust(1, 0, 0, 0);
+  private static ColorAdjust blueColor = new ColorAdjust(0.5, 0, 0, 0);
+
+  private static ColorAdjust redColor = new ColorAdjust(-0.39, 0, 0, 0);
 
   public static void cycleBottomLeft() {
 
     switch (cycleNumber) {
       case 1:
         // if current color is purple, move to red
-        visibleHelper("#bottomLeftUnclickedPurple", false);
-        visibleHelper("#bottomLeftUnclickedRed", true);
+        visibleHelper("#bottomLeftUnclicked", redColor);
         cycleNumber = 2;
         break;
       case 2:
         // if current color is red, move to blue
-        visibleHelper("#bottomLeftUnclickedRed", false);
-        visibleHelper("#bottomLeftUnclickedBlue", true);
+        visibleHelper("#bottomLeftUnclicked", blueColor);
         cycleNumber = 3;
 
         break;
       case 3:
         // if current color is blue, move to orange
-        visibleHelper("#bottomLeftUnclickedBlue", false);
-        visibleHelper("#bottomLeftUnclickedOrange", true);
+        visibleHelper("#bottomLeftUnclicked", greenColor);
         cycleNumber = 4;
         break;
       case 4:
         // if current color is orange, move to purple
-        visibleHelper("#bottomLeftUnclickedOrange", false);
-        visibleHelper("#bottomLeftUnclickedPurple", true);
+        visibleHelper("#bottomLeftUnclicked", purpleColor);
         cycleNumber = 1;
         break;
     }
   }
 
-  public static void visibleHelper(String id, boolean visibility) {
-    SceneManager.getPanel(AppPanel.THRUSTER).lookup(id).setVisible(visibility);
+  public static void visibleHelper(String id, Effect color) {
+    SceneManager.getPanel(AppPanel.THRUSTER).lookup(id).setEffect(color);
+    ;
   }
 }
--- src/main/java/nz/ac/auckland/se206/controllers/ThrusterController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ThrusterController.java
@@ -5,44 +5,14 @@ import javafx.scene.image.ImageView;
 
 public class ThrusterController {
 
-  @FXML private ImageView bottomLeftUnclickedPurple;
-  @FXML private ImageView bottomLeftClickedPurple;
-  @FXML private ImageView bottomLeftUnclickedOrange;
-  @FXML private ImageView bottomLeftClickedOrange;
-  @FXML private ImageView bottomLeftUnclickedBlue;
-  @FXML private ImageView bottomLeftClickedBlue;
-  @FXML private ImageView bottomLeftUnclickedRed;
-  @FXML private ImageView bottomLeftClickedRed;
+  @FXML private ImageView bottomLeftUnclicked;
+  @FXML private ImageView bottomLeftClicked;
 
-  public void setBottomLeftPurpleVisible() {
-    bottomLeftUnclickedPurple.setVisible(true);
+  public void setBottomLeftVisible() {
+    bottomLeftUnclicked.setVisible(true);
   }
 
-  public void setBottomLeftPurpleInvisible() {
-    bottomLeftUnclickedPurple.setVisible(false);
-  }
-
-  public void setBottomLeftOrangeVisible() {
-    bottomLeftUnclickedOrange.setVisible(true);
-  }
-
-  public void setBottomLeftOrangeInvisible() {
-    bottomLeftUnclickedOrange.setVisible(false);
-  }
-
-  public void setBottomLeftBlueVisible() {
-    bottomLeftUnclickedBlue.setVisible(true);
-  }
-
-  public void setBottomLeftBlueInvisible() {
-    bottomLeftUnclickedBlue.setVisible(false);
-  }
-
-  public void setBottomLeftRedVisible() {
-    bottomLeftUnclickedRed.setVisible(true);
-  }
-
-  public void setBottomLeftRedInvisible() {
-    bottomLeftUnclickedRed.setVisible(false);
+  public void setBottomLeftInvisible() {
+    bottomLeftUnclicked.setVisible(false);
   }
 }
