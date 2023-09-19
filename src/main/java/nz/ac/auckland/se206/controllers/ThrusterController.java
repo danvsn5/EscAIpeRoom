6a856fdf92b5394fc852246b492f1812d31520a6
dvas511 <dvas511@aucklanduni.ac.nz>
2023-09-20 03:10:48 +1200

    added clickable button functionality for all colors for bottom left button

--- src/main/java/nz/ac/auckland/se206/controllers/ThrusterController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ThrusterController.java
@@ -7,6 +7,12 @@ public class ThrusterController {
 
   @FXML private ImageView bottomLeftUnclickedPurple;
   @FXML private ImageView bottomLeftClickedPurple;
+  @FXML private ImageView bottomLeftUnclickedOrange;
+  @FXML private ImageView bottomLeftClickedOrange;
+  @FXML private ImageView bottomLeftUnclickedBlue;
+  @FXML private ImageView bottomLeftClickedBlue;
+  @FXML private ImageView bottomLeftUnclickedRed;
+  @FXML private ImageView bottomLeftClickedRed;
 
   public void setBottomLeftPurpleVisible() {
     bottomLeftUnclickedPurple.setVisible(true);
@@ -14,5 +20,29 @@ public class ThrusterController {
 
   public void setBottomLeftPurpleInvisible() {
     bottomLeftUnclickedPurple.setVisible(false);
+  }
+
+  public void setBottomLeftOrangeVisible() {
+    bottomLeftUnclickedOrange.setVisible(true);
+  }
+
+  public void setBottomLeftOrangeInvisible() {
+    bottomLeftUnclickedOrange.setVisible(false);
+  }
+
+  public void setBottomLeftBlueVisible() {
+    bottomLeftUnclickedBlue.setVisible(true);
+  }
+
+  public void setBottomLeftBlueInvisible() {
+    bottomLeftUnclickedBlue.setVisible(false);
+  }
+
+  public void setBottomLeftRedVisible() {
+    bottomLeftUnclickedRed.setVisible(true);
+  }
+
+  public void setBottomLeftRedInvisible() {
+    bottomLeftUnclickedRed.setVisible(false);
   }
 }
