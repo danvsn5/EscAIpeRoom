352e8d1a6295fee05954f9816216a787d87a551b
dvas511 <dvas511@aucklanduni.ac.nz>
2023-09-21 00:13:26 +1200

    tree flashing is now implemented in every room and follows along between rooms

--- src/main/java/nz/ac/auckland/se206/TreeAvatar.java
+++ src/main/java/nz/ac/auckland/se206/TreeAvatar.java
@@ -9,7 +9,7 @@ import nz.ac.auckland.se206.SceneManager.AppPanel;
 public class TreeAvatar {
 
   public static Timeline treeFlash =
-      new Timeline(new KeyFrame(Duration.seconds(1), e -> flashTree()));
+      new Timeline(new KeyFrame(Duration.millis(750), e -> flashTree()));
   private static int treeState = 0;
   private static Glow glowDim = new Glow(0.0);
   private static Glow glowBright = new Glow(0.7);
@@ -22,9 +22,20 @@ public class TreeAvatar {
   public static void flashTree() {
     if (treeState == 0) {
       SceneManager.getPanel(AppPanel.MAIN_ROOM).lookup("#miniTree").setEffect(glowBright);
+      SceneManager.getPanel(AppPanel.WORK).lookup("#miniTree").setEffect(glowBright);
+      SceneManager.getPanel(AppPanel.OUTSIDE).lookup("#miniTree").setEffect(glowBright);
+      SceneManager.getPanel(AppPanel.THRUSTER).lookup("#miniTree").setEffect(glowBright);
+      SceneManager.getPanel(AppPanel.CHEST).lookup("#miniTree").setEffect(glowBright);
+      SceneManager.getPanel(AppPanel.PROGRESS).lookup("#miniTree").setEffect(glowBright);
+
       treeState = 1;
     } else {
       SceneManager.getPanel(AppPanel.MAIN_ROOM).lookup("#miniTree").setEffect(glowDim);
+      SceneManager.getPanel(AppPanel.WORK).lookup("#miniTree").setEffect(glowDim);
+      SceneManager.getPanel(AppPanel.OUTSIDE).lookup("#miniTree").setEffect(glowDim);
+      SceneManager.getPanel(AppPanel.THRUSTER).lookup("#miniTree").setEffect(glowDim);
+      SceneManager.getPanel(AppPanel.CHEST).lookup("#miniTree").setEffect(glowDim);
+      SceneManager.getPanel(AppPanel.PROGRESS).lookup("#miniTree").setEffect(glowDim);
       treeState = 0;
     }
   }
