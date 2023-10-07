c3cb6bc73bb49f633bf04907a0209f9e1b8e1f0f
dvas511 <dvas511@aucklanduni.ac.nz>
2023-10-07 20:14:59 +1300

    Revert "Merge pull request #54 from SOFTENG206-2023/fix/sandNotShown"
    This reverts commit a80e8cfb948c510f68c1b8ca4d0cb79b4d05882a, reversing
    changes made to e35cbadec215dfdb5064a87d2ebf5472568d293a.

--- src/main/java/nz/ac/auckland/se206/controllers/ChatController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ChatController.java
@@ -265,37 +265,34 @@ public class ChatController {
             // if riddle was solved correctly, then -1 is added to the inventory; -2 is determined
             // from the launch panel and checks whether or not text to speech will be active
 
-            // If the first riddle is not solved, check if the player guesses correctly
             if (!GameState.firstRiddleSolved) {
               System.out.println("first riddle not solved");
-              // Check if the player gussess correctly
               if (lastMsg.getRole().equals("assistant")
                   && lastMsg.getContent().startsWith("Correct")) {
                 if (!GameState.firstRiddleSolved && GameState.missionList.contains(2)) {
-                  // If the player guesses correctly and its fuel mission, show the fuel and
-                  // increase stage
                   GameState.missionManager.getMission(MISSION.FUEL).increaseStage();
                   GameState.progressBarGroup.updateProgressOne(MISSION.FUEL);
                   System.out.println("Fuel Mission 1 Complete");
                   fuel.setDisable(false);
                   fuel.setVisible(true);
-                } else if (!GameState.firstRiddleSolved && GameState.missionList.contains(1)) {
-                  // If the player guesses correctly and its window mission, show the sand and
-                  // increase stage
-                  GameState.missionManager.getMission(MISSION.WINDOW).increaseStage();
-                  GameState.progressBarGroup.updateProgressOne(MISSION.WINDOW);
-                  System.out.println("Window riddle solved");
-                  sand.setDisable(false);
-                  sand.setVisible(true);
                 }
-                GameState.firstRiddleSolved = true;
-
-                // Speak the correct message from the tree
-                GameState.speak(lastMsg.getContent());
-                System.out.println("first riddle solved");
               }
             }
-            // else if (GameState.firstRiddleSolved && !GameState.secondRiddleSolved) {
+
+            //     else if (!GameState.firstRiddleSolved && GameState.missionList.contains(1)) {
+            //       GameState.missionManager.getMission(MISSION.WINDOW).increaseStage();
+            //       GameState.progressBarGroup.updateProgressOne(MISSION.WINDOW);
+            //       System.out.println("Window riddle solved");
+            //       sand.setDisable(false);
+            //       sand.setVisible(true);
+            //     }
+            //     GameState.firstRiddleSolved = true;
+
+            //     // Speak the correct message from the tree
+            //     GameState.speak(lastMsg.getContent());
+            //     System.out.println("first riddle solved");
+            //   }
+            // } else if (GameState.firstRiddleSolved && !GameState.secondRiddleSolved) {
             //   if (lastMsg.getRole().equals("assistant")
             //       && lastMsg.getContent().startsWith("Correct")) {
             //     GameState.secondRiddleSolved = true;
