4b5a9af70a196d0b34e215ceef9f7063b0c4a701
dvas511 <dvas511@aucklanduni.ac.nz>
2023-10-08 16:17:09 +1300

    added threading to all items in launchGame method such that the background video no longer freezes

--- src/main/java/nz/ac/auckland/se206/controllers/LaunchController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LaunchController.java
@@ -74,6 +74,7 @@ public class LaunchController {
 
   // clears all instances of existing rooms, wipes out the inventory and resets the timeline
   public void launchGame(MouseEvent ev) throws IOException {
+
     mediaPlayerOne.setOnEndOfMedia(
         new Runnable() {
           @Override
@@ -91,80 +92,91 @@ public class LaunchController {
             App.setUi(AppPanel.MAIN_ROOM);
           }
         });
-    Random rand = new Random();
-    int task1 = rand.nextInt(2) + 1;
-    int task2 = rand.nextInt(2) + 3;
-    GameState.missionListA.add(task1);
-    GameState.missionListA.add(task2);
 
     SceneManager.clearMap();
-    SceneManager.addPanel(AppPanel.MAIN_ROOM, loadFxml("mainRoom"));
+    Task<Void> initialiseTask =
+        new Task<Void>() {
 
-    SceneManager.addPanel(AppPanel.OUTSIDE, loadFxml("outsideRoom"));
-    SceneManager.addPanel(AppPanel.LOSE, loadFxml("loseRoom"));
-    SceneManager.addPanel(AppPanel.WIN, loadFxml("winRoom"));
-    SceneManager.addPanel(AppPanel.LAUNCH, loadFxml("startRoom"));
-    SceneManager.addPanel(AppPanel.THRUSTER, loadFxml("thrusterRoom"));
-    SceneManager.addPanel(AppPanel.CHEST, loadFxml("chest"));
-    SceneManager.addPanel(AppPanel.PROGRESS, loadFxml("progressBars"));
-    SceneManager.addPanel(AppPanel.STORAGE, loadFxml("storage"));
-    SceneManager.addPanel(AppPanel.CHAT, loadFxml("chat"));
+          @Override
+          protected Void call() throws Exception {
+            Random rand = new Random();
+            int task1 = rand.nextInt(2) + 1;
+            int task2 = rand.nextInt(2) + 3;
+            GameState.missionListA.add(task1);
+            GameState.missionListA.add(task2);
+            SceneManager.addPanel(AppPanel.MAIN_ROOM, loadFxml("mainRoom"));
+            SceneManager.addPanel(AppPanel.OUTSIDE, loadFxml("outsideRoom"));
+            SceneManager.addPanel(AppPanel.LOSE, loadFxml("loseRoom"));
+            SceneManager.addPanel(AppPanel.WIN, loadFxml("winRoom"));
+            SceneManager.addPanel(AppPanel.LAUNCH, loadFxml("startRoom"));
+            SceneManager.addPanel(AppPanel.THRUSTER, loadFxml("thrusterRoom"));
+            SceneManager.addPanel(AppPanel.CHEST, loadFxml("chest"));
+            SceneManager.addPanel(AppPanel.PROGRESS, loadFxml("progressBars"));
+            SceneManager.addPanel(AppPanel.STORAGE, loadFxml("storage"));
+            SceneManager.addPanel(AppPanel.CHAT, loadFxml("chat"));
 
-    createTimer();
+            createTimer();
 
-    GameState.createRandomColorNumber();
+            GameState.createRandomColorNumber();
 
-    GameState.missionManager.addMission(task1);
-    GameState.missionManager.addMission(task2);
-    GameState.missionList.add(task1);
-    GameState.missionList.add(task2);
-    GameState.progressBarGroup.setMissionOne(task1);
-    GameState.progressBarGroup.setMissionTwo(task2);
-    missionInitialise.initialiseFirstMission(task1);
-    missionInitialise.initialiseSecondMission(task2);
+            GameState.missionManager.addMission(task1);
+            GameState.missionManager.addMission(task2);
+            GameState.missionList.add(task1);
+            GameState.missionList.add(task2);
+            GameState.progressBarGroup.setMissionOne(task1);
+            GameState.progressBarGroup.setMissionTwo(task2);
+            missionInitialise.initialiseFirstMission(task1);
+            missionInitialise.initialiseSecondMission(task2);
 
-    if (GameState.missionList.contains(1)) {
-      mission1 = "Fix the Window";
-    } else if (GameState.missionList.contains(2)) {
-      mission1 = "Charge the Fuel";
-    }
+            if (GameState.missionList.contains(1)) {
+              mission1 = "Fix the Window";
+            } else if (GameState.missionList.contains(2)) {
+              mission1 = "Charge the Fuel";
+            }
 
-    if (GameState.missionList.contains(3)) {
-      mission2 = "Fix the Controller";
-    } else if (GameState.missionList.contains(4)) {
-      mission2 = "Fix the Thruster";
-    }
+            if (GameState.missionList.contains(3)) {
+              mission2 = "Fix the Controller";
+            } else if (GameState.missionList.contains(4)) {
+              mission2 = "Fix the Thruster";
+            }
 
-    String text =
-        "Hey there, Seems like you have crush-landed \non this planet! "
-            + "\nWonder who I am?\n"
-            + "I am the mysterious tree of this planet.\n"
-            + "To leave, you should have to fix this ship.\nFirst, "
-            + mission1
-            + " and then "
-            + mission2
-            + ".\nFind me outside for the clue to the first step!";
+            String text =
+                "Hey there, Seems like you have crush-landed \non this planet! "
+                    + "\nWonder who I am?\n"
+                    + "I am the mysterious tree of this planet.\n"
+                    + "To leave, you should have to fix this ship.\nFirst, "
+                    + mission1
+                    + " and then "
+                    + mission2
+                    + ".\nFind me outside for the clue to the first step!";
 
-    ((Label) SceneManager.getPanel(AppPanel.MAIN_ROOM).lookup("#guideLabel")).setText(text);
+            ((Label) SceneManager.getPanel(AppPanel.MAIN_ROOM).lookup("#guideLabel")).setText(text);
 
-    String mission1;
+            String mission1;
 
-    if (GameState.missionList.contains(1)) {
-      mission1 =
-          "Know how to fix the window? I shall give you a riddle and the answer should guide you"
-              + " to the next step. You ready?\n";
-      ((TextArea) SceneManager.getPanel(AppPanel.CHAT).lookup("#chatTextArea"))
-          .appendText(mission1);
+            if (GameState.missionList.contains(1)) {
+              mission1 =
+                  "Know how to fix the window? I shall give you a riddle and the answer should"
+                      + " guide you to the next step. You ready?\n";
+              ((TextArea) SceneManager.getPanel(AppPanel.CHAT).lookup("#chatTextArea"))
+                  .appendText(mission1);
 
-    } else if (GameState.missionList.contains(2)) {
-      mission1 =
-          "Know how to charge the fuel? I shall give you a riddle and the answer should guide"
-              + " you to the next step. You ready?\n";
-      ((TextArea) SceneManager.getPanel(AppPanel.CHAT).lookup("#chatTextArea"))
-          .appendText(mission1);
-    }
+            } else if (GameState.missionList.contains(2)) {
+              mission1 =
+                  "Know how to charge the fuel? I shall give you a riddle and the answer should"
+                      + " guide you to the next step. You ready?\n";
+              ((TextArea) SceneManager.getPanel(AppPanel.CHAT).lookup("#chatTextArea"))
+                  .appendText(mission1);
+            }
 
-    SceneManager.setPrevious(AppPanel.MAIN_ROOM);
+            SceneManager.setPrevious(AppPanel.MAIN_ROOM);
+
+            return null;
+          }
+        };
+
+    Thread videoInitialisationThread = new Thread(initialiseTask);
+    videoInitialisationThread.start();
 
     // App.setUi(AppPanel.MAIN_ROOM);
   }
