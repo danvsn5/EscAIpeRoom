4a5ff5767f74a5752dc5a95a6c5873de61e2de72
dvas511 <dvas511@aucklanduni.ac.nz>
2023-10-11 19:59:44 +1300

    final video of ship flying in space now plays when game is beaten

--- src/main/java/nz/ac/auckland/se206/controllers/WinController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/WinController.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.SceneManager.AppPanel;
@@ -12,6 +15,17 @@ public class WinController {
 
   @FXML private Button returnMenuButton;
   @FXML private Button quitButton;
+  @FXML private MediaView flyingVideo;
+  private Media mediaOne;
+  private MediaPlayer mediaPlayerOne;
+
+  public void initialize() throws Exception {
+    mediaOne = new Media(App.class.getResource("/videos/launch/flyShip.mp4").toURI().toString());
+    mediaPlayerOne = new MediaPlayer(mediaOne);
+    flyingVideo.setMediaPlayer(mediaPlayerOne);
+    mediaPlayerOne.setCycleCount(MediaPlayer.INDEFINITE);
+    mediaPlayerOne.play();
+  }
 
   public void returnMenu() throws IOException {
     GameState.inventory.clear();
