cb7b782208c4f0fd351f1cd98a71efcbc1e36309
Xinlin Long <xlon323@aucklanduni.ac.nz>
2023-10-15 11:58:47 +1300

    Modified the onSendMessage method so that it now uses the new getResponse method. Detecting if gpt's response starts with Correct is now done in a seperate method

--- src/main/java/nz/ac/auckland/se206/controllers/ChatController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ChatController.java
@@ -258,115 +258,47 @@ public class ChatController {
       return;
     }
 
+    // Send message to gpt to ask for response
     Task<Void> typeCall =
         new Task<Void>() {
 
           @Override
           protected Void call() throws Exception {
-
+            // Start generation
             isGenerating = true;
+            // Get the response
+            ChatMessage response = getResponse(message, chatCompletionRequest);
+            // Append the response to text field and notebook
+            response.setRole("Wise Ancient Tree");
+            Platform.runLater(() -> appendChatMessage(response));
+            Platform.runLater(() -> appendToNotebook(response));
+            response.setRole("assistant");
 
-            ChatMessage msg = new ChatMessage("user", message);
-            msg.setRole("Me");
-            appendToNotebook(msg);
-            System.out.println(msg.getContent());
-            msg.setRole("user");
-            // appendChatMessageArea(thinkingMessage);
-            ChatMessage lastMsg = runGpt(msg);
-            lastMsg.setRole("Wise Ancient Tree");
-            Platform.runLater(() -> appendChatMessage(lastMsg));
-            appendToNotebook(lastMsg);
+            // Process the response from gpt, detect if it says correct
+            Platform.runLater(() -> processResponse(response.getContent()));
 
-            lastMsg.setRole("assistant");
-            System.out.println("lastMsg");
-            // if riddle was solved correctly, then -1 is added to the inventory; -2 is determined
-            // from the launch panel and checks whether or not text to speech will be active
-
-            if (!GameState.firstRiddleSolved) {
-              System.out.println("first riddle not solved");
-              // Check if the player gussess correctly
-              if (lastMsg.getRole().equals("assistant")
-                  && lastMsg.getContent().startsWith("Correct")) {
-                if (!GameState.firstRiddleSolved && GameState.missionList.contains(2)) {
-                  // If the player guesses correctly and its fuel mission, show the fuel and
-                  // increase stage
-                  GameState.missionManager.getMission(MISSION.FUEL).increaseStage();
-                  GameState.progressBarGroup.updateProgressOne(MISSION.FUEL);
-                  System.out.println("Fuel Mission 1 Complete");
-                  Platform.runLater(
-                      () -> {
-                        showFuel();
-                      });
-                } else if (!GameState.firstRiddleSolved && GameState.missionList.contains(1)) {
-                  // If the player guesses correctly and its window mission, show the sand and
-                  // increase stage
-                  GameState.missionManager.getMission(MISSION.WINDOW).increaseStage();
-                  GameState.progressBarGroup.updateProgressOne(MISSION.WINDOW);
-                  System.out.println("Window riddle solved");
-                  Platform.runLater(
-                      () -> {
-                        showSand();
-                      });
-                }
-                GameState.firstRiddleSolved = true;
-
-                // Speak the correct message from the tree
-                GameState.speak(lastMsg.getContent());
-                System.out.println("first riddle solved");
-              }
-            } else if (GameState.isFirstMissionCompleted && GameState.missionList.contains(4)) {
-              if (lastMsg.getRole().equals("assistant")
-                  && lastMsg.getContent().startsWith("Correct")) {
-                GameState.missionManager.getMission(MISSION.THRUSTER).increaseStage();
-                GameState.progressBarGroup.updateProgressTwo(MISSION.THRUSTER);
-                System.out.println("Thruster riddle solved");
-              }
-            }
-
-            //     else if (!GameState.firstRiddleSolved && GameState.missionList.contains(1)) {
-            //       GameState.missionManager.getMission(MISSION.WINDOW).increaseStage();
-            //       GameState.progressBarGroup.updateProgressOne(MISSION.WINDOW);
-            //       System.out.println("Window riddle solved");
-            //       sand.setDisable(false);
-            //       sand.setVisible(true);
-            //     }
-            //     GameState.firstRiddleSolved = true;
-
-            //     // Speak the correct message from the tree
-            //     GameState.speak(lastMsg.getContent());
-            //     System.out.println("first riddle solved");
-            //   }
-            // } else if (GameState.firstRiddleSolved && !GameState.secondRiddleSolved) {
-            //   if (lastMsg.getRole().equals("assistant")
-            //       && lastMsg.getContent().startsWith("Correct")) {
-            //     GameState.secondRiddleSolved = true;
-            //   }
-            // }
             updateProgress(1, 1);
             return null;
           }
         };
 
-    // loading.progressProperty().bind(typeCall.progressProperty());
-
     typeCall.setOnSucceeded(
         e -> {
+          // Stop generating
           isGenerating = false;
-          // Start talk
+          // Stop thinking animation and start talking
           smallBubble.setVisible(false);
           medBubble.setVisible(false);
           largeBubble.setVisible(false);
           bubbleTimeline.pause();
-          System.out.println("timeline should have stopped");
           bubbleVariable = 0;
-          // loading.progressProperty().unbind();
-          // loading.setVisible(false);
-          // loadingCircle.setFill(Color.valueOf("264f31"));
-          inputText.setDisable(false);
           startTalk();
+          // Activate input text field and hint button
+          inputText.setDisable(false);
           hintButton.setDisable(false);
         });
 
+    // Start the thread
     Thread typeInThread = new Thread(typeCall);
     typeInThread.start();
   }
@@ -383,6 +315,41 @@ public class ChatController {
         hintRectangle.setVisible(true);
         hintRectangle.setDisable(false);
       }
+    }
+  }
+
+  /* Process with the gpt's response, if it starts with correct, update the game progress and show relavent message/image */
+  private void processResponse(String response) {
+    // If the gpt is not answering with correct, return
+    if (!response.startsWith("Correct")) {
+      return;
+    }
+
+    // If the first riddle is not solved, mark it as solved
+    if (!GameState.firstRiddleSolved) {
+      if (GameState.missionList.contains(1)) {
+        // If the player guesses correctly and its window mission, show the sand and
+        // increase stage
+        GameState.missionManager.getMission(MISSION.WINDOW).increaseStage();
+        GameState.progressBarGroup.updateProgressOne(MISSION.WINDOW);
+        System.out.println("Window riddle solved");
+        showSand();
+      } else if (GameState.missionList.contains(2)) {
+        // If the player guesses correctly and its fuel mission, show the fuel and
+        // increase stage
+        GameState.missionManager.getMission(MISSION.FUEL).increaseStage();
+        GameState.progressBarGroup.updateProgressOne(MISSION.FUEL);
+        System.out.println("Fuel Mission 1 Complete");
+        showFuel();
+      }
+      // Speak the correct response from gpt and mark first riddle as solved
+      GameState.speak(response);
+      GameState.firstRiddleSolved = true;
+    } else if (GameState.missionList.contains(4)) {
+      // If it is currently second mission and it's thruster mission, increase stage
+      GameState.missionManager.getMission(MISSION.THRUSTER).increaseStage();
+      GameState.progressBarGroup.updateProgressTwo(MISSION.THRUSTER);
+      System.out.println("Thruster riddle solved");
     }
   }
 
