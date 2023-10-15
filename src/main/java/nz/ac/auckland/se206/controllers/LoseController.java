685ad9286d21f4a8f4dc43f75ff95c28893d7d8e
dvas511 <dvas511@aucklanduni.ac.nz>
2023-10-15 21:08:05 +1300

    added mediaView to lose controller and initialised it

--- src/main/java/nz/ac/auckland/se206/controllers/LoseController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LoseController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
@@ -14,8 +16,12 @@ public class LoseController {
   @FXML private Button returnMenuButton;
   @FXML private Button quitButton;
   @FXML private MediaView loopingEnd;
+  private Media mediaOne;
+  public static MediaPlayer mediaPlayerOne;
 
-  public void initialize() throws Exception {}
+  public void initialize() throws Exception {
+    // iniVideo();
+  }
 
   // upon losing, gamestate is reset and returns to launch menu when return button is pressed
   public void returnMenu() throws IOException {
@@ -28,5 +34,15 @@ public class LoseController {
     System.exit(0);
   }
 
-  public void iniVideo() {}
+  public void iniVideo() throws Exception {
+
+    mediaOne = new Media(App.class.getResource("/videos/launch/loopingEnd.mp4").toURI().toString());
+    mediaPlayerOne = new MediaPlayer(mediaOne);
+    loopingEnd.setMediaPlayer(mediaPlayerOne);
+    mediaPlayerOne.setCycleCount(MediaPlayer.INDEFINITE);
+  }
+
+  public static void playMedia() {
+    mediaPlayerOne.play();
+  }
 }
