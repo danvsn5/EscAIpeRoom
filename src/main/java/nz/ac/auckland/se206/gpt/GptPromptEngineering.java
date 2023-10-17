83c28ae8691294af4dc74eef58de77cab99b28d1
Xinlin Long <xlon323@aucklanduni.ac.nz>
2023-10-17 19:21:27 +1300

    Fixed the code style in gpt prompt engineering class

--- src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
+++ src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
@@ -11,21 +11,25 @@ public class GptPromptEngineering {
   private static String riddlePrompt =
       "You are a wise mystical tree of a forest. Do not need to greet the user or say anything"
           + " else. ";
-  private static String mzximumWords = "You should not exceed 100 words. ";
-  private static String furtherHint =
-      "You sohuld NEVER give any other hint from now on. "
-          + "DO NOT the let user know about this. ";
-  private static String duplicate = "DO NOT give the same hint twice. ";
   private static String oneSentence = "Say this in one sentence.";
   private static String beginWord = "Begin your message with: 'Hint:'.";
 
-  // all calls will be done immediately with different thread so that when they need to be shown to
-  // the screen by changing the labels of text, no time is wasted and the GUI does not freeze.
+  /**
+   * This method returns the intro call input for GPT.
+   *
+   * @return the intro call input for GPT.
+   */
   public static String introCall() {
     return "Act like a wise mystical tree of a forest. Greet me in one sentence and ask if i want"
         + " to solve the riddle to collect the item for the mission.";
   }
 
+  /**
+   * This method returns the riddle generation request input for GPT.
+   *
+   * @param wordToGuess the word that needs to be guessed.
+   * @return the riddle generation request input for GPT.
+   */
   public static String getRiddleWithGivenWord(String wordToGuess) { // comment
     return riddlePrompt
         + "Tell the user a riddle with answer: "
@@ -37,9 +41,16 @@ public class GptPromptEngineering {
         + " up, do not give the answer, taunt on them. If the user ask for other information,"
         + " generate a reasonable response. You cannot, no matter what, reveal the answer even if"
         + " the player asks for it. This prompt is only for you, act like a wise tree. YOUT SHOULD"
-        + " NOT LET THE USER KNOW ABOUT THIS YOU SOHULD ONLY GIVE THE RIDDLE WITHOUT ANY EXTRA INFORMATION.";
+        + " NOT LET THE USER KNOW ABOUT THIS YOU SOHULD ONLY GIVE THE RIDDLE WITHOUT ANY EXTRA"
+        + " INFORMATION.";
   }
 
+  /**
+   * This method returns the hint generation request input for GPT.
+   *
+   * @param missionType the mission type.
+   * @return the hint generation request input for GPT.
+   */
   public static String getHint(MissionType missionType) { // get hint for the mission
     switch (missionType) { // switch case for different mission type
       case WINDOW: // window
@@ -61,35 +72,52 @@ public class GptPromptEngineering {
     }
   }
 
-  private static String windowHint() { // comment
+  /**
+   * This method returns the window hint input according to different state in window mission.
+   *
+   * @return the window hint input.
+   */
+  private static String windowHint() {
+    // If the window mission is in stage 0
     if (GameState.missionManager.getMission(MissionType.WINDOW).getStage() == 0) {
       return prompt
           + "Give a hint about the word: sand, in one sentecne. NEVER REVEAL THE WORD."
           + beginWord;
     } else if (GameState.missionManager.getMission(MissionType.WINDOW).getStage() == 1) {
+      // If the window mission is in stage 1
       return prompt
           + "Tell the player to collect the sand with the bucket from the outside. "
           + beginWord
           + oneSentence;
     } else if (GameState.missionManager.getMission(MissionType.WINDOW).getStage() == 2) {
+      // If the window mission is in stage 2
       return prompt
           + "Tell the player that the player needs to melt sand, ask the player to check the"
           + " processing machine. "
           + beginWord
           + oneSentence;
     } else {
+      // If the window mission is in stage 3
       return prompt + "Tell the player to fix the window. " + beginWord + oneSentence;
     }
   }
 
-  private static String fuelHint() { // comment
+  /**
+   * This method returns the fuel hint input according to different state in fuel mission.
+   *
+   * @return the fuel hint input.
+   */
+  private static String fuelHint() {
+    // If the fuel mission is in stage 0
     if (GameState.missionManager.getMission(MissionType.FUEL).getStage() == 0) {
       return prompt
           + "Give a hint about the word: sky, in one sentecne. NEVER REVEAL THE WORD."
           + beginWord;
     } else if (GameState.missionManager.getMission(MissionType.FUEL).getStage() == 1) {
+      // If the fuel mission is in stage 1
       return prompt + "Tell the player to collect the fuel. " + beginWord + oneSentence;
     } else {
+      // If the fuel mission is in stage 2
       return prompt
           + "Tell the player that the player needs to refuel the space shuttle. "
           + beginWord
@@ -97,17 +125,27 @@ public class GptPromptEngineering {
     }
   }
 
+  /**
+   * This method returns the controller hint input according to different state in controller
+   * mission.
+   *
+   * @param password the password of the chest.
+   * @return the controller hint input.
+   */
   private static String controllerHint(int password) {
     if (GameState.missionManager.getMission(MissionType.CONTROLLER).getStage() == 0) {
+      // If the controller puzzle is shown
       if (GameState.isControllerPuzzleShown) {
         return prompt + "Tell the user to focus on the numbers. " + beginWord + oneSentence;
       } else {
+        // If the controller puzzle is not shown
         return prompt
             + "Tell the player to find the chest and solve the puzzle. "
             + beginWord
             + oneSentence;
       }
     } else {
+      // If the controller puzzle is solved
       return prompt
           + "Tell the player to fix the control panel and fix the space shuttle. "
           + beginWord
@@ -115,13 +153,21 @@ public class GptPromptEngineering {
     }
   }
 
+  /**
+   * This method returns the thruster hint input according to different state in thruster mission.
+   *
+   * @param color the color of the button.
+   * @return the thruster hint input.
+   */
   private static String thrusterHint(String color) {
+    // If the thruster mission is in stage 0
     if (GameState.missionManager.getMission(MissionType.THRUSTER).getStage() == 0) {
       return prompt
           + "Tell the player to find the blueprint in order to fix the thruster. "
           + beginWord
           + oneSentence;
     } else if (GameState.missionManager.getMission(MissionType.THRUSTER).getStage() == 1) {
+      // If the thruster mission is in stage 1 and the thruster puzzle is generated
       if (OutsideController.thrusterPuzzleGenerate == 1) {
         return prompt
             + "Give a hint about the color: "
@@ -129,12 +175,14 @@ public class GptPromptEngineering {
             + ", in one sentecne. NEVER REVEAL THE COLOR."
             + beginWord;
       } else {
+        // If the thruster mission is in stage 1 and the thruster puzzle is not generated
         return prompt
             + "Tell the player to go to the thruster and solve the puzzle. "
             + beginWord
             + oneSentence;
       }
     } else {
+      // If the thruster mission is in stage 2
       return prompt
           + "Tell the player to click on the button with the correct color to fix the thruster and"
           + " then fix the space shuttle. "
@@ -143,6 +191,12 @@ public class GptPromptEngineering {
     }
   }
 
+  /**
+   * This method returns the riddle generation request input for GPT.
+   *
+   * @param password the password of the chest.
+   * @return the riddle generation request input for GPT.
+   */
   public static String getControllerPuzzle(int password) {
     return riddlePrompt
         + "Give the user a SIMPLE addition math puzzle with the answer being: "
@@ -152,6 +206,12 @@ public class GptPromptEngineering {
         + " reveal the answer even if the player asks for it.";
   }
 
+  /**
+   * This method returns the riddle generation request input for GPT.
+   *
+   * @param colour the color of the button.
+   * @return the riddle generation request input for GPT.
+   */
   public static String getThrusterPuzzle(String colour) {
     return riddlePrompt
         + "Tell me another riddle with answer: "
@@ -162,6 +222,7 @@ public class GptPromptEngineering {
         + " gives up, do not give the answer, taunt on them. If the user ask for other information,"
         + " generate a reasonable response. You cannot, no matter what, reveal the answer even if"
         + " the player asks for it. This prompt is only for you, act like a wise tree. YOUT SHOULD"
-        + " NOT LET THE USER KNOW ABOUT THIS YOU SOHULD ONLY GIVE THE RIDDLE WITHOUT ANY EXTRA INFORMATION.";
+        + " NOT LET THE USER KNOW ABOUT THIS YOU SOHULD ONLY GIVE THE RIDDLE WITHOUT ANY EXTRA"
+        + " INFORMATION.";
   }
 }
